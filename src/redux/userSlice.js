a98f290a8ebfe7a5980cad19a8bc82756b608e22
Tomas Sanchez <tms90@outlook.es>
2022-05-22 19:10:52 +0200

    Added userSlice.js and store.js in redux file

---
+++ src/redux/userSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {},
+  reducers: {
+    logIn: (user, action) => ({ ...action.payload }),
+  },
+});
+export const { logIn: loginActionCreator } = userSlice.actions;
+
+export default userSlice.reducer;
